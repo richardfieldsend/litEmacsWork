20b5ff98b73a0ae9464176c0eba51abb0bab12c1
Richard Fieldsend <rf343@cam.ac.uk>
2019-11-03 00:55:47 +0000

    Modified init.el to remove entry to use-package

--- init.el
+++ init.el
@@ -17,7 +17,6 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;(package-initialize)
 
 (org-babel-load-file (concat user-emacs-directory "config.org"))
 (custom-set-variables
@@ -26,7 +25,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ecb-options-version "2.50")
- '(package-selected-packages '(use-package)))
+ '(package-selected-packages
+   '(tomatinho slime typing edit-server diary-manager bbdb lorem-ipsum ebib auctex emmet-mode writeroom-mode paredit tagedit projectile cider clojure-mode-extra-font-locking clojure-mode ein py-autopep8 flycheck elpy centered-cursor-mode all-the-icons-dired alarm-clock achievements helm-spotify-plus multiple-cursors palimpsest ledger-mode helm wotd magit ghub with-editor magit-popup diminish dired-git-info fireplace pomidor org-mind-map org-bullets minimap doom-themes doom-modeline all-the-icons rainbow-delimiters zenburn-theme use-package)))
+; '(package-selected-packages '(use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
